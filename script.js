1b6ca220fae56e833fbabb991ad9483718c68cab
firuz <you@example.com>
2022-10-04 16:42:33 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.swiper1', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
